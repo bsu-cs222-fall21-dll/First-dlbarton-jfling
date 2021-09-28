0530a93b2a9d2c50e327b6753c00ce3006720d22
lingj <jfling@bsu.edu>
2021-09-27 23:07:50 -0400

    adds User Input class

---
+++ src/main/java/edu/bsu/main/UI.java
@@ -0,0 +1,11 @@
+package edu.bsu.main;
+
+public class UI {
+    public String WikiUserInterface(){
+        return String.format("Search for Article:");
+    }
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+}
--- src/main/java/edu/bsu/main/main.java
+++
@@ -1,7 +0,0 @@
-package edu.bsu.main;
-
-public class main {
-    public static void main(String[] args){
-
-    }
-}
